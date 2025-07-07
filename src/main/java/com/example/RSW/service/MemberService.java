93d20d229a66cb2e352e462cc0819c34552e6f86
doomdex84 <doomdex84@gmail.com>
2025-07-07 12:24:20 +0900

    리포지터리 세팅

--- src/main/java/com/example/RSW/service/MemberService.java
+++ src/main/java/com/example/RSW/service/MemberService.java
@@ -59,6 +59,7 @@ public class MemberService {
 
 		existsMember = getMemberByNameAndEmail(name, email);
 
+		
 		if (existsMember != null) {
 			return ResultData.from("F-8", Ut.f("이미 사용중인 이름(%s)과 이메일(%s)입니다", name, email));
 		}
