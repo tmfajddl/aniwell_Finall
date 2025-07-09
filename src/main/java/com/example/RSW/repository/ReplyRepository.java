a8efdbb79b367f607e1a1be9bebc1fed0e8cbfd8
ryuj7338 <ryuj7338@gmail.com>
2025-07-09 22:23:39 +0900

    댓글 확인

--- src/main/java/com/example/RSW/repository/ReplyRepository.java
+++ src/main/java/com/example/RSW/repository/ReplyRepository.java
@@ -9,5 +9,15 @@ import com.example.RSW.vo.Reply;
 @Mapper
 public interface ReplyRepository {
 
-    public List<Reply> getForPrintReplies(String relTypeCode, int relId);
+    public List<Reply> getForPrintReplies(int loginedMemberId, String relTypeCode, int relId);
+
+    public void writeReply(int loginedMemberId, String body, String relTypeCode, int relId);
+
+    public int getLastInsertId();
+
+    public Reply getReply(int id);
+
+    public void modifyReply(int id, String body);
+
+    void deleteReply(int id);
 }
