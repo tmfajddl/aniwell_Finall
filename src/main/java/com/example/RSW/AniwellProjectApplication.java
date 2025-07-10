70da181338b8c4b12ebeebe51143431c4d1d45c4
qkjm1 <kqk9269@gmail.com>
2025-07-10 11:24:07 +0900

    test

--- src/main/java/com/example/RSW/AniwellProjectApplication.java
+++ src/main/java/com/example/RSW/AniwellProjectApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication  // ← 이 애노테이션이 핵심입니다!
 public class AniwellProjectApplication {
 
-	
+
     public static void main(String[] args) {
         SpringApplication.run(AniwellProjectApplication.class, args);
     }
