61ac8b1b25f2e76fdb747f16f88f7aad40bb954b
tmfajddl <tmfajddl12@gmail.com>
2025-07-18 16:18:03 +0900

    게시글 상세보기 / 관리자 페이지

--- src/main/java/com/example/RSW/controller/UsrReplyController.java
+++ src/main/java/com/example/RSW/controller/UsrReplyController.java
@@ -15,6 +15,8 @@ import com.example.RSW.vo.Rq;
 
 import jakarta.servlet.http.HttpServletRequest;
 
+import java.util.List;
+
 @Controller
 public class UsrReplyController {
 
@@ -30,68 +32,56 @@ public class UsrReplyController {
     // 댓글 작성 처리
     @RequestMapping("/usr/reply/doWrite")
     @ResponseBody
-    public String doWrite(HttpServletRequest req, String relTypeCode, int relId, String body) {
+    public ResultData<Reply> doWrite(HttpServletRequest req, String relTypeCode, int relId, String body) {
         Rq rq = (Rq) req.getAttribute("rq");
 
         if (Ut.isEmptyOrNull(body)) {
-            return Ut.jsHistoryBack("F-2", "내용을 입력해주세요");
+            return ResultData.from("F-2", "내용을 입력해주세요");
         }
 
-        // 댓글 저장
         ResultData writeReplyRd = replyService.writeReply(rq.getLoginedMemberId(), body, relTypeCode, relId);
         int id = (int) writeReplyRd.getData1();
+        Reply reply = replyService.getReply(id);
 
-        // 게시글 상세 페이지로 이동
-        return Ut.jsReplace(writeReplyRd.getResultCode(), writeReplyRd.getMsg(), "../article/detail?id=" + relId);
+        return ResultData.from("S-1", "댓글이 등록되었습니다", reply);
     }
 
-    // 댓글 수정 처리
+    @RequestMapping("/usr/reply/list")
+    @ResponseBody
+    public List<Reply> getReplies(String relTypeCode, int relId) {
+        return replyService.getForPrintReplies(rq.getLoginedMemberId(), relTypeCode, relId);
+    }
+
     @RequestMapping("/usr/reply/doModify")
     @ResponseBody
-    public String doModify(HttpServletRequest req, int id, String body) {
-        System.err.println(id);
-        System.err.println(body);
-
+    public ResultData<String> doModify(HttpServletRequest req, int id, String body) {
         Rq rq = (Rq) req.getAttribute("rq");
         Reply reply = replyService.getReply(id);
 
         if (reply == null) {
-            return Ut.jsHistoryBack("F-1", Ut.f("%d번 댓글은 존재하지 않습니다", id));
+            return ResultData.from("F-1", Ut.f("%d번 댓글은 존재하지 않습니다", id));
         }
 
-        // 수정 권한 체크
-        ResultData loginedMemberCanModifyRd = replyService.userCanModify(rq.getLoginedMemberId(), reply);
-        if (loginedMemberCanModifyRd.isSuccess()) {
-            replyService.modifyReply(id, body);
-        }
+        ResultData canModify = replyService.userCanModify(rq.getLoginedMemberId(), reply);
+        if (canModify.isFail()) return canModify;
 
-        reply = replyService.getReply(id); // 최신 내용 재조회
-        return reply.getBody(); // 새로 수정된 댓글 본문 반환 (AJAX 용)
+        replyService.modifyReply(id, body);
+        return ResultData.from("S-1", "댓글이 수정되었습니다");
     }
 
-    // 댓글 삭제 처리
     @RequestMapping("/usr/reply/doDelete")
     @ResponseBody
-    public String doDelete(@RequestParam int id,
-                           @RequestParam String relTypeCode,
-                           @RequestParam int relId,
-                           @RequestParam int boardId) {
-
+    public ResultData<Integer> doDelete(HttpServletRequest req, int id) {
+        Rq rq = (Rq) req.getAttribute("rq");
         Reply reply = replyService.getReply(id);
-        if (reply == null) {
-            return Ut.jsHistoryBack("F-1", "해당 댓글이 존재하지 않습니다.");
-        }
 
-        // 삭제 권한 체크
-        ResultData actorCanDeleteRd = replyService.userCanDelete(rq.getLoginedMemberId(), reply);
-        if (actorCanDeleteRd.isFail()) {
-            return Ut.jsHistoryBack(actorCanDeleteRd.getResultCode(), actorCanDeleteRd.getMsg());
-        }
+        if (reply == null) return ResultData.from("F-1", "해당 댓글이 존재하지 않습니다.");
+
+        ResultData canDelete = replyService.userCanDelete(rq.getLoginedMemberId(), reply);
+        if (canDelete.isFail()) return canDelete;
 
         replyService.deleteReply(id);
-
-        // 삭제 후 게시글 상세 페이지로 리다이렉트
-        return Ut.jsReplace("S-1", "댓글을 삭제했습니다.",
-                "../article/detail?id=" + relId + "&boardId=" + boardId);
+        return ResultData.from("S-1", "댓글이 삭제되었습니다", id);
     }
+
 }
