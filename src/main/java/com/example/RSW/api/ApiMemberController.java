006448d1b4f882513006fecb654254582867fdf5
qkjm1 <kqk9269@gmail.com>
2025-07-24 12:07:14 +0900

    크루리스트 띄우기

--- src/main/java/com/example/RSW/api/ApiMemberController.java
+++ src/main/java/com/example/RSW/api/ApiMemberController.java
@@ -1,21 +1,29 @@
 package com.example.RSW.api;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.RSW.service.MemberService;
 import com.example.RSW.vo.Member;
 import com.example.RSW.vo.Rq;
 
 import jakarta.servlet.http.HttpServletRequest;
 
+
 @RestController
 @RequestMapping("/api/member")
 public class ApiMemberController {
+	
+	
+	 @Autowired
+	    private MemberService memberService;
 
-    @GetMapping("/myPage")
+//로그인된 유저 api
+	@GetMapping("/myPage")
     public ResponseEntity<Member> getMyPage(HttpServletRequest req) {
         Rq rq = (Rq) req.getAttribute("rq");
         Member loginedMember = rq.getLoginedMember();
@@ -26,6 +34,24 @@ public class ApiMemberController {
 
         return ResponseEntity.ok(loginedMember);
     }
+
+	// id기준 유저 api
+    @GetMapping("/getUsrInfo")
+    public ResponseEntity<Member> getUsrInfo(HttpServletRequest req, int memberId ) {
+    	
+    	 Rq rq = (Rq) req.getAttribute("rq");
+         Member loginedMember = rq.getLoginedMember();
+
+         if (loginedMember == null) {
+             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+         }
+         
+        Member member = memberService.getMemberById(memberId);
+
+        return ResponseEntity.ok(member);
+    }
+    
+    
 }    
 
 
