42f5d3a9570119680d8e9ed3daca9b56f3791396
tmfajddl <tmfajddl12@gmail.com>
2025-07-26 15:42:10 +0900

    ble/로드셀 웹소켓 연결

--- src/main/java/com/example/RSW/repository/PetHealthRepository.java
+++ src/main/java/com/example/RSW/repository/PetHealthRepository.java
@@ -3,6 +3,7 @@ package com.example.RSW.repository;
 import com.example.RSW.vo.PetHealthLog;
 import org.apache.ibatis.annotations.Mapper;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Map;
 
@@ -10,5 +11,7 @@ import java.util.Map;
 public interface PetHealthRepository {
     void insertLog(PetHealthLog log);  // Map 대신 VO 사용
     List<PetHealthLog> findLogsByPetId(int petId);
+
+    List<PetHealthLog> findByPetIdAndDate(int petId, LocalDate date);
 }
 
