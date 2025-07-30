c3e48cdeb5f5215306bea477e2fc18770e39c9b5
tmfajddl <tmfajddl12@gmail.com>
2025-07-30 11:13:24 +0900

    공지 삭제되면 삭제 되도록

--- src/main/java/com/example/RSW/service/NotificationService.java
+++ src/main/java/com/example/RSW/service/NotificationService.java
@@ -190,4 +190,8 @@ public class NotificationService {
 	public void deleteAllByMemberId(int loginedMemberId) {
 		notificationRepository.deleteAllByMemberId(loginedMemberId);
 	}
+
+	public void deleteByLink(String link) {
+		notificationRepository.deleteByLink(link);
+	}
 }
