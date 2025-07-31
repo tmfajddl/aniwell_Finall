013802ca97d91243eddf355dcca985db4b048e51
qkjm1 <kqk9269@gmail.com>
2025-07-31 13:29:09 +0900

    웹페이지 높이 너비 고정 반응형시도

--- src/main/resources/static/resource/js/calendar.js
+++ src/main/resources/static/resource/js/calendar.js
@@ -11,13 +11,14 @@ function renderMiniCalendar(targetId, events, date = new Date()) {
 	const today = new Date();
 
 	const englishMonthNames = [
-		"JANUARY", "FEBRUARY", "MARCH", "APRIL", "MAY", "JUNE",
-		"JULY", "AUGUST", "SEPTEMBER", "OCTOBER", "NOVEMBER", "DECEMBER"
+		"JANUARY 1", "FEBRUARY 2", "MARCH 3", "APRIL 4", "MAY 5", "JUNE 6",
+		"JULY 7", "AUGUST 8", "SEPTEMBER 9", "OCTOBER 10", "NOVEMBER 11", "DECEMBER 12"
 	];
 
 	const monthLabel = document.getElementById('current-month-label');
 	if (monthLabel) {
 		monthLabel.textContent = englishMonthNames[month];
+		monthLabel.classList.add('font-bold');
 	}
 
 	const header = document.createElement('div');
