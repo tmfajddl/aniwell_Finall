746400270db1691499a56ef529cd50034901c971
tmfajddl <tmfajddl12@gmail.com>
2025-08-12 13:14:24 +0900

    병원방문기록 Service / Repository 추가

---
+++ src/main/java/com/example/RSW/repository/VisitRepository.java
@@ -0,0 +1,19 @@
+package com.example.RSW.repository;
+
+import com.example.RSW.vo.Visit;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.net.InterfaceAddress;
+import java.util.List;
+
+@Mapper
+public interface VisitRepository {
+
+    int insertVisit(Visit visit);
+
+    int updateVisit(Visit visit);
+
+    void deleteVisit(int id);
+
+    List<Visit> selectVisitsByPetId(int petId);
+}
